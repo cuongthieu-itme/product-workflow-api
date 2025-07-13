2355e909e3f254289455c8903c97ffa138297dfb
cuongthieume <thieutrancuong.me@gmail.com>
2025-07-13 15:29:51 +0700

    update

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -2,16 +2,13 @@ import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
-  @ApiProperty({
-    example: '',
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @MinLength(1)
   name: string;
 
   @ApiProperty({
-    example: '',
     required: false,
   })
   @IsOptional()
--- src/category/dto/filter-category.dto.ts
+++ src/category/dto/filter-category.dto.ts
@@ -4,7 +4,6 @@ import { Transform } from 'class-transformer';
 
 export class FilterCategoryDto {
   @ApiProperty({
-    example: '',
     required: false,
   })
   @IsOptional()
@@ -12,7 +11,6 @@ export class FilterCategoryDto {
   name?: string;
 
   @ApiProperty({
-    example: '',
     required: false,
   })
   @IsOptional()
@@ -20,7 +18,6 @@ export class FilterCategoryDto {
   description?: string;
 
   @ApiProperty({
-    example: 1,
     required: false,
   })
   @IsOptional()
@@ -29,7 +26,6 @@ export class FilterCategoryDto {
   page?: number;
 
   @ApiProperty({
-    example: 10,
     required: false,
   })
   @IsOptional()
