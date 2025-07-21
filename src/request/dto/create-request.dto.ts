b41c56e27da1c6ab69e98557929c3bef25f50c80
cuongthieu <cuongtt@solashi.com>
2025-07-21 11:02:08 +0700

    update

--- src/request/dto/create-request.dto.ts
+++ src/request/dto/create-request.dto.ts
@@ -86,6 +86,14 @@ export class CreateRequestDto {
   status?: RequestStatus;
 
   @ApiProperty({
+    required: false,
+  })
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  statusProductId?: number;
+
+  @ApiProperty({
     type: [RequestMaterialDto],
     required: false,
   })
