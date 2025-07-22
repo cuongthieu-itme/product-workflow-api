f6ee3d2200e098a2bc44852d66fa8986019bcc98
cuongthieume <thieutrancuong.me@gmail.com>
2025-07-22 22:20:55 +0700

    update

--- src/notification-admin/dto/update-is-read.dto.ts
+++ src/notification-admin/dto/update-is-read.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { ArrayNotEmpty, IsArray, IsInt } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsInt, IsDefined } from 'class-validator';
+
+import { Type } from 'class-transformer';
 
 export class UpdateIsReadDto {
   @ApiProperty({ type: [Number], example: [1, 2, 3] })
+  @IsDefined()
   @IsArray()
   @ArrayNotEmpty()
   @IsInt({ each: true })
+  @Type(() => Number)
   ids: number[];
 }
