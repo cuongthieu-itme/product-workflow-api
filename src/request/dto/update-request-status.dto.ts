c3c009c48e15836de88883a99b00b17d5f714aa4
cuongthieu <cuongtt@solashi.com>
2025-07-23 16:52:54 +0700

    update

--- src/request/dto/update-request-status.dto.ts
+++ src/request/dto/update-request-status.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { RequestStatus } from '@prisma/client';
 import { IsEnum, IsOptional, IsNumber } from 'class-validator';
 
 export class UpdateRequestStatusDto {
+  @ApiProperty({
+    enum: RequestStatus,
+  })
   @IsEnum(RequestStatus)
   status: RequestStatus;
 
+
+  @ApiProperty({
+    type: Number,
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
   statusProductId?: number;
