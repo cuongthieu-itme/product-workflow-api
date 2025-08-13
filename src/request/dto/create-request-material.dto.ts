59210d82f9b973ecd6e42d66fc5160b7e3028e43
cuongthieume <thieutrancuong.me@gmail.com>
2025-08-13 23:28:45 +0700

    ok

--- src/request/dto/create-request-material.dto.ts
+++ src/request/dto/create-request-material.dto.ts
@@ -8,63 +8,87 @@ import {
   Min,
   IsNotEmpty,
 } from 'class-validator';
-import { MaterialType } from '@prisma/client';
+import { MaterialType, RequestMaterialStatus } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateNewMaterialDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNumber()
   @Min(1)
   quantity: number;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   unit: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   image?: string[];
 
+  @ApiProperty()
   @IsEnum(MaterialType)
   type: MaterialType;
 
+  @ApiProperty()
   @IsNumber()
   originId: number;
 
+  @ApiProperty()
+  @IsEnum(RequestMaterialStatus)
+  status: RequestMaterialStatus;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
+  price?: number;
+
+  @ApiProperty({ required: false })
   @IsOptional()
   requestInput?: CreateMaterialRequestInputDto;
 }
 
 export class CreateMaterialRequestInputDto {
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsNumber()
   @Min(0)
   quantity?: number;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsDateString()
   expectedDate?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   supplier?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   sourceCountry?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsNumber()
   @Min(0)
   price?: number;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   reason?: string;
